d6ffe5fbe84765839a29099c74c03b056c3e6379
JEIN JEON <101503824+realCCC@users.noreply.github.com>
2022-06-13 13:44:02 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ window.addEventListener('scroll', _.throttle(function () {
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
-fadeEls.forEach(function(fadeEl, index){
+fadeEls.forEach(function (fadeEl, index) {
   //gsap.to(요소, 지속시간, 옵션)
   gsap.to(fadeEl, 1, {
     delay: (index + 1) * .7, //0.7, 1.4, 2.1, 2.7
@@ -68,9 +68,65 @@ new Swiper('.promotion .swiper', {
     el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
     clickable: true // 사용자의 페이지 번호 요소 제어
   },
-  navigation:{
+  navigation: {
     prevEl: '.promotion .swiper-prev',
     nextEl: '.promotion .swiper-next'
   }
 })
 
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false; // isHidePromotion ->> Promotion 이 숨겨져있나요?  false는 보이고 있다는 의미
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  // !가 붙어있는 것은 반대가 되게 해주세요 라는 의미  let 으로 false가 되어있으니 true 값이 됨.
+  if (isHidePromotion) {
+    // 숨김 처리!
+    promotionEl.classList.add('');
+  } else {
+    // 보임 처리!
+    promotionEl.classList.remove('hide');
+  }
+});
+
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector, //선택자
+    random(1.5, 2.5), //애니메이션 동작 시간
+    { // 옵션
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay)
+    });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, //보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+})
